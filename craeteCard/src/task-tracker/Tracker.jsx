2c127afa8e90e7fdbc274a2fde54fe64e267849e
padma67 <60962682+padma67@users.noreply.github.com>
2020-06-28 16:05:41 +0530

    Update Tracker.jsx

--- craeteCard/src/task-tracker/Tracker.jsx
+++ craeteCard/src/task-tracker/Tracker.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Card from "./Card";
 
-const Tracker = ({ days, createTask, toggleTask }) => {
+const Tracker = ({ days, createTask, toggleTask,remove,removeTask }) => {
   return (
     <div className="main-container">
       {days.map((day, index) => (
@@ -10,6 +10,8 @@ const Tracker = ({ days, createTask, toggleTask }) => {
           day={day}
           createTask={(value) => createTask(index, value)}
           toggleTask={(taskIndex) => toggleTask(index, taskIndex)}
+          remove={()=>remove(index)}
+          removeTask={(taskIndex)=> removeTask(index,taskIndex)}
         />
       ))}
     </div>
